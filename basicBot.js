03b90949b384d06287f101b6801a2b9b363805f8
rucocohero <cocoru@gmx.de>
2017-06-01 14:13:55 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -923,7 +923,7 @@
             var timeElapsed = API.getTimeElapsed();
 
             if (basicBot.settings.voteSkip) {
-                if ((mehs - woots) >= (basicBot.settings.voteSkipLimit)) {
+                if ((mehs) >= (basicBot.settings.voteSkipLimit)) {
                     API.sendChat(subChat(basicBot.chat.voteskipexceededlimit, {name: dj.username, limit: basicBot.settings.voteSkipLimit}));
                     if (basicBot.settings.smartSkip && timeLeft > timeElapsed){
                         basicBot.roomUtilities.smartSkip();
